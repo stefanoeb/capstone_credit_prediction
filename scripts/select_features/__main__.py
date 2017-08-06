f2d6888fa8ed21b7ca7b9a5534f40e62f4acefca
Stefano Bourscheid <stefano.bourscheid@gmail.com>
2017-08-06 01:39:43 -0300

    damn good score already :fire:

--- scripts/select_features/__main__.py
+++ scripts/select_features/__main__.py
@@ -1,18 +1,25 @@
 import pandas as pd
 import os
-import datetime
+from datetime import date
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 df = pd.read_csv(dir_path + '/raw.csv')
 
+
+def calculaIdade(floatDate):
+    if not floatDate > 0:
+        return 0
+    strDate = str(floatDate)
+    dateObject = date(year=int(strDate[0:4]), month=int(strDate[4:6]), day=int(strDate[6:8]))
+    return date.today().year - dateObject.year
+
+
 variaveis_extrair = [
     # contratos.csv
-    'data_venda',
     'faturado',
     'valor_entrada',
     'qtd_parcelas',
-    'primeiro_vencimento',
     'valor_financiado',
     'valor_parcela',
     'mda',
@@ -40,7 +47,9 @@ variaveis_extrair = [
     ]
 
 df = df[variaveis_extrair]
-df = df.rename(index=str, columns={'mda.1': 'mda_cliente'})
+df['data_nascimento'] = df['data_nascimento'].apply(calculaIdade)
+df['data_admissao'] = df['data_admissao'].apply(calculaIdade)
+df = df.rename(index=str, columns={'mda.1': 'mda_cliente', 'data_admissao': 'anos_empresa', 'data_nascimento': 'idade'})
 
 df.to_csv(path_or_buf='data/out.csv',
           sep=';')
